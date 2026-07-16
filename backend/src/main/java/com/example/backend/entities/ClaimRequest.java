7dbe52fb7c338ff51f944cbe4b29d3dc45e07f3e
KrishCodesw <krishjain.w@gmail.com>
2026-07-17 01:30:30 +0530

    Added ClaimRequest model

---
+++ backend/src/main/java/com/example/backend/entities/ClaimRequest.java
@@ -0,0 +1,40 @@
+package com.example.backend.entities;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "claim_requests")
+public class ClaimRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManytoOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManytoOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = claimant_id)
+    private User claimant;
+
+    @Column(columnDefinition = "TEXT")
+    private String proofDescription;
+    private String status;
+
+    public ClaimRequest(){}
+
+    public Long getId(){return id;}
+    public void setId(Long id){this.id=id;}
+
+    public Item getItem(){return item;}
+    public void setItem(Item item){this.item=item;}
+
+    public User getClaimant(){return claimant;}
+    public void setClaimant(User claimant){this.claimant=claimant;}
+
+    public String getProofDescription(){return proofDescription;}
+    public void setProofDescription(String proofDescription){this.proofDescription=proofDescription;}
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+}
