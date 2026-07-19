22edd5e9a62422ec28c20321595a8e8f5eab365c
KrishCodesw <krishjain.w@gmail.com>
2026-07-19 11:31:19 +0530

    Added report endpoint and matches endpoint

--- backend/src/main/java/com/example/backend/controllers/ItemController.java
+++ backend/src/main/java/com/example/backend/controllers/ItemController.java
@@ -5,19 +5,28 @@ import com.example.backend.services.ItemService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/items")
 @CrossOrigin(origins="http://localhost:5173")
 public class ItemController {
 
-//    public ItemController(ItemService itemService){
-//        this.itemService=itemService;
-//    }
-//
-//    @PostMapping("/report")
-//    public ResponseEntity<Item> reportItem(@RequestBody Item item){
-//        Item savedItem=itemService.reportItem(item);
-//        return ResponseEntity.ok(savedItem);
-//    }
+    private final ItemService itemservice;
 
+    public ItemController(ItemService itemservice) {
+        this.itemservice = itemservice;
+    }
+
+    @PostMapping("/report")
+    public ResponseEntity<Item> reportItem(@RequestBody Item item){
+        Item savedItem =itemservice.reportItem(item);
+        return ResponseEntity.ok(savedItem);
+    }
+
+    @PostMapping("/matches")
+    public ResponseEntity<Item> matchItem(@RequestBody Item lostItem){
+    List<Item> matches=itemservice.findSuggestedMatches(lostItem);
+    return ResponseEntity.ok((Item) matches);
+    }
 }
